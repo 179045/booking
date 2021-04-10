4ab8fb1bc8c75e4bbe72ae47efdba8496f79fdb0
taibek <taibek.kair@gmail.com>
2021-04-10 13:15:01 +0600

    CRUD Company

---
+++ backend/views/company/company/_form.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\company\Company */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="company-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'is_del')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
